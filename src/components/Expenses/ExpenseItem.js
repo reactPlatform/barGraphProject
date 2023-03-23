9a6b9f35d206e0325ab62e0d34f0d5f9937d2ee5
kavi <sumathi291098@gmail.com>
2023-03-23 18:55:10 +0530

    Change Title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
+import React,{ useState } from 'react';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDetails from './ExpenseDetails';
 import ExpenseDate from './ExpenseDate';
 const ExpenseItem = (props) => {
-    const deleteExp = (event) => {
-        event.target.parentElement.remove();
+    const [title, setTitle] = useState(props.title);
+    const changeTitle = () => {
+        setTitle('Updated Title');
     }
     return(
         <Card className="expense-item">
            <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
-            <button onClick={deleteExp} className='button'>Delete Expense</button>
+            <ExpenseDetails title={title} amount={props.amount} location={props.location}/>
+            <button onClick={changeTitle} className='button'>Change Title</button>
         </Card>
     );
 }
