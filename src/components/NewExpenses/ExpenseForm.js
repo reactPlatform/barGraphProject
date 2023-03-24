de25373c63fc3cae8e56362459c656c9e9a00362
kavi <sumathi291098@gmail.com>
2023-03-24 10:03:15 +0530

    Submit the form

--- src/components/NewExpenses/ExpenseForm.js
+++ src/components/NewExpenses/ExpenseForm.js
@@ -5,8 +5,23 @@ const ExpenseForm = () => {
     const [enteredTitle,setTitle] = useState('');
     const [enteredAmount,setAmount] = useState('');
     const [enteredDate,setDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount:'',
+    //     enteredDate:''
+    // })
     const titleChangeHandler = (event) => {
         setTitle(event.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle:event.target.value
+
+        // })
+
+        // setUserInput((prevState) => {
+        //     return {...prevState, enteredTitle:event.target.value}
+        // })
+
     };
     const amountChangeHandler = (event) => {
         setAmount(event.target.value);
@@ -14,8 +29,17 @@ const ExpenseForm = () => {
     const dateChangeHandler = (event) => {
         setDate(event.target.value);
     };
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expensedata = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expensedata);
+    };
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
